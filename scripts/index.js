74aa6ffb63d700c501f7c9ab9592e6779718e0c1
Jaclyn Yang <jackiey321@gmail.com>
2022-05-09 03:46:17 -0400

    fixed home layer icon positioning

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 $(window).on('shown.bs.offcanvas', function (e) {
-  $('.home-layers').css('top', '115px');
+  $('.home-layers').css('top', '105px');
 });
 
 $(window).on('hide.bs.offcanvas', function (e) {
-  $('.home-layers').css('top', '550px');
+  $('.home-layers').css('top', '490px');
 });
